6e99de172dead242c286a3d9f003eaa00307c2f2
MunSul-alsayegh <135947099+MunSul-alsayegh@users.noreply.github.com>
2023-10-30 07:37:34 +0300

    Add files via upload

---
+++ test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by Munsulaiman on 15/04/1445 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
